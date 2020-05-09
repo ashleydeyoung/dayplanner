d7d6e9f7abad80e6b363aa186b32db5859bbf534
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-05-08 22:45:29 -0400

    get local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,36 +1,43 @@
-
-var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)`
-var currentHour = new Date().getHours();//Current hour in military time
+var currentDateAndTime = Date(Date.now()); // exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)`
+var currentHour = 10// new Date().getHours(); //Current hour in military time
 
 console.log("working");
 
 var savedBtn = $(".btn");
+
 $("#currentDay").text(currentDateAndTime);
 
-function updateHours() {
 
-    for (var i = 9; i < 18; i++) {
-        if (currentHour > i) {
-            $("#hour-" + i).addClass("past")
-        }
-        else if (currentHour === i) {
-            $("#hour-" + i).removeClass("past").addClass("present")
-        }
-        else {
-            $("#hour-" + i).removeClass("past").removeClass("present").addClass("future")
-        }
-        
-    }
+for (var i = 9; i < 18; i++) {
+  var currentHourText = localStorage.getItem("hour"+i);
+  
+  if (currentHourText !== null) {
+    $("#textArea"+i).val(currentHourText);
+  }
+  if (currentHour > i) {
+    $("#hour-" + i).addClass("past");
+  } else if (currentHour === i) {
+    $("#hour-" + i)
+      .removeClass("past")
+      .addClass("present");
+  } else {
+    $("#hour-" + i)
+      .removeClass("past")
+      .removeClass("present")
+      .addClass("future");
+  }
 }
 
-updateHours();
-
-savedBtn.on("click", function(){
-
-    console.log($(this).val());
-    var time = $(this).val();
-    localStorage.setItem("hour" + time, $("#textArea" + time).val().trim());
-})
+savedBtn.on("click", function () {
+  console.log($(this).val());
+  var time = $(this).val();
+  localStorage.setItem(
+    "hour" + time,
+    $("#textArea" + time)
+      .val()
+      .trim()
+  );
+});
 
 // function init() {
 //     for
